889edcb335de3911dac554a8f1ae52112b42a190
Jamie O'Neill <jamieoneill@users.noreply.github.com>
2019-04-24 11:52:23 +0100

    Update getSN.js

--- functions/getSN.js
+++ functions/getSN.js
@@ -29,7 +29,8 @@ exports.handler = function(event, context, callback){
           "Authorization": "Basic ${TOKEN}",
       },
     }).done(function (response, textStatus, xhr) {
-      send(response);
+      var jsonResult = xmlToJson(response);
+      send(jsonResult);
     }).fail(function (xhr, textStatus, errorThrown) {
       send(xhr);
     });
@@ -41,3 +42,44 @@ exports.handler = function(event, context, callback){
     getInc();
   }
 }
+
+
+
+// Changes XML to JSON
+function xmlToJson(xml) {
+	
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
