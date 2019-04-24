5f8e1ce35d0df02b2975a56c203870831b9da7f0
Jamie O'Neill <jamieoneill@users.noreply.github.com>
2019-04-24 14:28:28 +0100

    Update getSN.js

--- functions/getSN.js
+++ functions/getSN.js
@@ -1,88 +1,84 @@
+const axios = require('axios');
 
-//const axios = require('axios');
+exports.handler = function (event, context, callback) {
+    const {
+        SN_TICKET,
+        TOKEN
+    } = process.env;
 
-exports.handler = function(event, context, callback){
-  const { SN_TICKET, TOKEN } = process.env;
+    // Send user response
+    const send = body => {
+        callback(null, {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+            },
+            body: JSON.stringify(body)
+        });
+    }
 
-  // Send user response
-  const send = body => {
-    callback(null, {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers':
-          'Origin, X-Requested-With, Content-Type, Accept'
-      },
-      body: JSON.stringify(body)
-    });
-  }
-  
-  const getInc = () => {
-    $.ajax({
-      url: "https://aigm3.service-now.com/IncidentAPI.do?SOAP=",
-      async: false,
-      type: "POST",
-      data: "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:inc=\"http://www.service-now.com/IncidentServiceAPI\">\r\n   <soapenv:Header/>\r\n   <soapenv:Body>\r\n      <inc:IncidentAPIRequest>\r\n         <snTicketNumber>${SN_TICKET}</snTicketNumber>\r\n         <sourceSystem>Test</sourceSystem>\r\n         <operation>GetRecord</operation>\r\n      </inc:IncidentAPIRequest>\r\n   </soapenv:Body>\r\n</soapenv:Envelope>",
-      headers: {
-          "Content-Type": "text/xml",
-          "Access-Control-Allow-Origin": "*",
-          "Authorization": "Basic ${TOKEN}",
-      },
-    }).done(function (response, textStatus, xhr) {
-      var jsonResult = xmlToJson(response);
-	setTimeout("console.log('waiting')", 3000);
-      send(jsonResult);
-	    	setTimeout("console.log('waiting')", 3000);
+    const getInc = () => {
 
-    }).fail(function (xhr, textStatus, errorThrown) {
-      send(xhr);
-    });
-  }
+        let xmls = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:inc=\"http://www.service-now.com/IncidentServiceAPI\">\r\n   <soapenv:Header/>\r\n   <soapenv:Body>\r\n      <inc:IncidentAPIRequest>\r\n         <snTicketNumber>${SN_TICKET}</snTicketNumber>\r\n         <sourceSystem>Test</sourceSystem>\r\n         <operation>GetRecord</operation>\r\n      </inc:IncidentAPIRequest>\r\n   </soapenv:Body>\r\n</soapenv:Envelope>";
 
-  // Make sure method is GET
-  if(event.httpMethod == 'GET') {
-    // Run
-    getInc();
-  }
+        axios.post('https://aigm3.service-now.com/IncidentAPI.do?SOAP=',
+                xmls, {
+                    headers: {
+                        "Content-Type": "text/xml",
+                        "Access-Control-Allow-Origin": "*",
+                        "Authorization": "Basic ${TOKEN}",
+                    }
+                }).then(res =>
+                send(xmlToJson(res)))
+            .catch(err => send(err));
+
+    }
+
+    // Make sure method is GET
+    if (event.httpMethod == 'GET') {
+        // Run
+        getInc();
+    }
 }
 
 
 
 // Changes XML to JSON
 function xmlToJson(xml) {
-	
-	// Create the return object
-	var obj = {};
 
-	if (xml.nodeType == 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
-		obj["@attributes"] = {};
-			for (var j = 0; j < xml.attributes.length; j++) {
-				var attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			}
-		}
-	} else if (xml.nodeType == 3) { // text
-		obj = xml.nodeValue;
-	}
+    // Create the return object
+    var obj = {};
 
-	// do children
-	if (xml.hasChildNodes()) {
-		for(var i = 0; i < xml.childNodes.length; i++) {
-			var item = xml.childNodes.item(i);
-			var nodeName = item.nodeName;
-			if (typeof(obj[nodeName]) == "undefined") {
-				obj[nodeName] = xmlToJson(item);
-			} else {
-				if (typeof(obj[nodeName].push) == "undefined") {
-					var old = obj[nodeName];
-					obj[nodeName] = [];
-					obj[nodeName].push(old);
-				}
-				obj[nodeName].push(xmlToJson(item));
-			}
-		}
-	}
-	return obj;
+    if (xml.nodeType == 1) { // element
+        // do attributes
+        if (xml.attributes.length > 0) {
+            obj["@attributes"] = {};
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3) { // text
+        obj = xml.nodeValue;
+    }
+
+    // do children
+    if (xml.hasChildNodes()) {
+        for (var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof (obj[nodeName]) == "undefined") {
+                obj[nodeName] = xmlToJson(item);
+            } else {
+                if (typeof (obj[nodeName].push) == "undefined") {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xmlToJson(item));
+            }
+        }
+    }
+    return obj;
 };
