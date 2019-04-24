c0ee9f6cb5adbd468d6189cbc1aac1a79c52a963
Jamie O'Neill <jamieoneill@users.noreply.github.com>
2019-04-24 11:24:38 +0100

    Update getusers.js

--- functions/getusers.js
+++ functions/getusers.js
@@ -17,6 +17,26 @@ exports.handler = function(event, context, callback){
       body: JSON.stringify(body)
     });
   }
+  
+  const getInc = () => {
+    $.ajax({
+      url: "https://aigm3.service-now.com/IncidentAPI.do?SOAP=",
+      async: true,
+      type: "POST",
+      data: "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:inc=\"http://www.service-now.com/IncidentServiceAPI\">\r\n   <soapenv:Header/>\r\n   <soapenv:Body>\r\n      <inc:IncidentAPIRequest>\r\n         <snTicketNumber>${SN_TICKET}</snTicketNumber>\r\n         <sourceSystem>Test</sourceSystem>\r\n         <operation>GetRecord</operation>\r\n      </inc:IncidentAPIRequest>\r\n   </soapenv:Body>\r\n</soapenv:Envelope>",
+      headers: {
+          "Content-Type": "text/xml",
+          "Access-Control-Allow-Origin": "*",
+          "Authorization": "Basic ${TOKEN}",
+      },
+    }).done(function (response, textStatus, xhr) {
+      console.log(response);
+      send(response)
+    }).fail(function (xhr, textStatus, errorThrown) {
+      console.log(xhr);
+      send(xhr)
+    });
+  }
 
   // Perform API call
   const getUsers = () => {
@@ -29,5 +49,6 @@ exports.handler = function(event, context, callback){
   if(event.httpMethod == 'GET') {
     // Run
     getUsers();
+    getInc();
   }
 }
